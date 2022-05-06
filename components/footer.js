9c278f9ff730244073da0eb9d32c1aa27b178f7d
Vrushabh Veer <veervrushabh@gmail.com>
2022-05-06 20:15:09 +0530

    Tralive

---
+++ components/footer.js
@@ -0,0 +1,34 @@
+function footer(){
+
+    return `
+    <div class="footer">
+            <div class="footer-left">
+                <p>Address</p>
+                <p>
+                    5th flora, 700/D kings road, green
+                    lane New York-1782
+                    +10 367 826 2567
+                    contact@carpenter.com
+                </p>
+            </div>
+            <div class="footer-center">
+                <p>Home</p>
+                <p>About</p>
+                <p>Contact</p>
+                <p>Destinations</p>
+            </div>
+            <div class="footer-right">
+                <p>Follow Us</p>
+                <div class="icons">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-youtube"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-pinterest"></i>
+                </div>
+            </div>
+        </div>
+        <div id="bottom">Copyright ©2022 All rights reserved | Made By Vrushabh Veer.</div>`;
+}
+
+export default footer;
